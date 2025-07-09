3d1c12ef38d12894b75a5dcd15c9781e706ebc0b
Tim <microwavehead@gmail.com>
2025-07-09 19:59:03 +0100

    Initial API Setup

---
+++ EnsekReader/EnsekReader.API/Controllers/FileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnsekReader.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpPost("Upload")]
+        public void Upload()
+        { }
+    }
+}
